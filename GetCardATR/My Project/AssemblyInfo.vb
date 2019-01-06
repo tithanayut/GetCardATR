ff3635ba980650ca780fb63cbc1de58c3709235e
tithanayut <30551284+tithanayut@users.noreply.github.com>
2019-01-06 19:21:22 +0700

    Initial commit

---
+++ GetCardATR/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GetCardATR")>
+<Assembly: AssemblyDescription("Software for retrieving Smart Card ATR (Answer to Reset) using PC/SC Library")>
+<Assembly: AssemblyCompany("Thanayut T.")>
+<Assembly: AssemblyProduct("GetCardATR")>
+<Assembly: AssemblyCopyright("Copyright ©  2019 Thanayut T.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0d868ea6-75c5-4f29-8624-14c912bb7a47")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ GetCardATR/SmartPCSC.vb
@@ -0,0 +1,45 @@
+﻿Imports PCSC
+Imports PCSC.Utils
+
+Public Class SmartPCSC
+
+    Dim contextFactory As IContextFactory = PCSC.ContextFactory.Instance
+    Dim hContext As ISCardContext
+
+    Dim reader As SCardReader
+
+    Public Function getReaderList()
+        Dim readerList As String()
+
+        Try
+            hContext = contextFactory.Establish(SCardScope.System)
+            readerList = hContext.GetReaders()
+            hContext.Release()
+
+            Return readerList
+        Catch ex As Exceptions.PCSCException
+            Throw New Exception("Error: getReaderList() : " & ex.Message & " (" & ex.SCardError.ToString() & ")")
+        End Try
+    End Function
+
+    Public Function getCardATR(ByVal readerName As String)
+        hContext = contextFactory.Establish(SCardScope.System)
+        reader = New SCardReader(hContext)
+
+        If readerName = Nothing Then
+            Throw New ArgumentException("Error: getCardATR() : No card reader specified")
+        End If
+
+        Dim connection As SCardError
+        connection = reader.Connect(readerName, SCardShareMode.Shared, SCardProtocol.T0 Or SCardProtocol.T1)
+        If connection <> SCardError.Success Then
+            Throw New Exception("Error: getCardATR() : Connection fail (" & SCardHelper.StringifyError(connection) & ")")
+        End If
+
+        Dim sc = hContext.ConnectReader(readerName, SCardShareMode.Shared, SCardProtocol.T0 Or SCardProtocol.T1)
+        Dim cardAtr As Byte() = sc.GetAttrib(SCardAttribute.AtrString)
+
+        Return cardAtr
+    End Function
+
+End Class
